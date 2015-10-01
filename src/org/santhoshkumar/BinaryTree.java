d0f7597ed4e1b6add0b7a4d2f0596197632f5bad
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-01 21:53:35 +0530

    size, width, height and depth implementation

--- src/org/santhoshkumar/BinaryTree.java
+++ src/org/santhoshkumar/BinaryTree.java
@@ -6,11 +6,24 @@ import java.util.*;
 public class BinaryTree {
 
     Node root;
+    int maxDepth;
 
     public static void main(String[] args){
         BinaryTree bt = new BinaryTree();
         bt.createTree();
+        System.out.println("In Order Traversal");
         bt.inOrder();
+        System.out.println();
+        System.out.println("Pre Order Traversal");
+        bt.preOrder();
+        System.out.println();
+        System.out.println("Post Order Traversal");
+        bt.postOrder();
+        System.out.println();
+        System.out.println("Height of Tree: "+bt.getHeight(bt.root));
+        System.out.println("Depth of Tree: "+bt.getDepth(bt.root));
+        System.out.println("Width of Tree: "+bt.getMaxWidth());
+        System.out.println("Size of Tree: "+bt.getSize(bt.root));
     }
 
     public void createTree(){
@@ -41,7 +54,7 @@ public class BinaryTree {
                 current = current.left;
             }else if (!stack.isEmpty()){
                 current = stack.pop();
-                System.out.println(current.data);
+                System.out.print(current.data + " ");
                 current = current.right;
             }
         }
@@ -53,12 +66,118 @@ public class BinaryTree {
             return;
         }
 
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            Node current = stack.pop();
+            System.out.print(current.data + " ");
+
+            if(current.right != null){
+                stack.push(current.right);
+            }
+            if(current.left != null){
+                stack.push(current.left);
+            }
+        }
     }
 
     public void postOrder(){
         if(root == null){
             return;
         }
+
+        Stack<Node> stack = new Stack<Node>();
+        Node current = root;
+        while(true){
+            if(current!= null){
+                if(current.right != null){
+                    stack.push(current.right);
+                }
+                stack.push(current);
+                current = current.left;
+                continue;
+            }
+
+            if (stack.isEmpty()) {
+                return;
+            }
+
+            current = stack.pop();
+
+            if(current.right != null && !stack.isEmpty() && current.right == stack.peek()){
+                stack.pop();
+                stack.push(current);
+                current = current.right;
+            }else{
+                System.out.print(current.data + " ");
+                current = null;
+            }
+        }
+    }
+
+    public int getHeight(Node node){
+        //Leaf node are at height 0;
+        if(node == null){
+            return -1;
+        }
+        else if(node.right == null && node.left == null){
+            return 0;
+        }
+        return Math.max(getHeight(node.left), getHeight(node.right))+1;
+    }
+
+    public int getDepth(Node node){
+        this.maxDepth = 0;
+        findMaxDepth(node, 0);
+        return maxDepth;
+    }
+
+    private void findMaxDepth(Node node, int depth){
+        //root node is at depth 0;
+        if(node != null) {
+            if (depth > maxDepth) {
+                maxDepth = depth;
+            }
+            findMaxDepth(node.left, depth + 1);
+            findMaxDepth(node.right, depth+1);
+        }
+    }
+
+    public int getMaxWidth(){
+        // Largest BFS node chain length
+        if(root == null){
+            return 0;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        int maxWidth = 0;
+        int width = 0;
+        while(!queue.isEmpty()){
+            width = queue.size();
+            if (width > maxWidth){
+                maxWidth = width;
+            }
+            while(width > 0){
+                Node node = queue.poll();
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                width--;
+            }
+        }
+        return maxWidth;
+    }
+
+    public int getSize(Node node){
+        //no of nodes -> size of tree
+        if (node == null){
+            return 0;
+        }
+        return (1+getSize(node.right)+getSize(node.left));
     }
 
 }
