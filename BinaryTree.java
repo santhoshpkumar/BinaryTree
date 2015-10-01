c4e9782bdc41bd9f15bf02702c6890e4681ef3af
Santhosh Kumar Prakash <skcodeworks@users.noreply.github.com>
2015-10-01 19:20:53 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,8 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public static BinaryTree {
+public class BinaryTree {
         public static void main(String[] args){
                 System.out.println("Binary Tree Implementation");
+        }
+}
+
+class Node{
+        Node left;
+        Node right;
+        int data;
+        
+        Node(){
+                data = -1;
+        }
+        
+        Node(int data){
+                this.data = data;
         }
 }
