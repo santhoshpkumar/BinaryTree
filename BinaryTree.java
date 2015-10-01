00e926a8e4414cfa6ae1e41ae28e673e47b8cbf1
Santhosh Kumar Prakash <skcodeworks@users.noreply.github.com>
2015-10-01 19:18:14 +0530

    Create Binary Tree Class

---
+++ BinaryTree.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public static BinaryTree {
+        public static void main(String[] args){
+                System.out.println("Binary Tree Implementation");
+        }
+}
